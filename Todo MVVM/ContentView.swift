5310a5b3e4f4d5918447b06d37fd0d40114ed769
Wender Patrick <outrowender@gmail.com>
2022-03-23 16:45:25 -0300

    Initial Commit

---
+++ Todo MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo MVVM
+//
+//  Created by Wender Patrick on 23/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo MVVM/Todo_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_MVVMApp.swift
+//  Todo MVVM
+//
+//  Created by Wender Patrick on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
